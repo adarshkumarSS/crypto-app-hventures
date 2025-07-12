1246806c1cc82a8f0c419dbf3c6d5ee069790efe
adarsh <adarsh@victopialabs.com>
2025-07-12 19:36:45 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StatusBar, StyleSheet, useColorScheme } from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text style={styles.text}>Hello, Crypto Adarsh!</Text>
+    </View>
+  );
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
